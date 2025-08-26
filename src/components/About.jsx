4380d530e30e33fe7e24d74ce32efbd4c288e4ae
sumit <sumitkundu10012001@gmail.com>
2025-08-27 02:40:32 +0530

    updated about image

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,4 +1,4 @@
-import aboutImg from "../assets/about.jpg"
+import aboutImg from "../assets/about2.jpg"
 import { ABOUT_TEXT } from "../constants";
 import { motion } from "framer-motion";
 const About = () => {
@@ -9,17 +9,32 @@ const About = () => {
         <span className="text-neutral-500"> Me</span>
       </h2>
       <div className="flex flex-wrap">
-        <motion.div
+        {/* <motion.div
           whileInView={{ opacity: 1, x: 0 }}
           initial={{ x: -100, opacity: 0 }}
           // animate={{ x: 0, opacity: 1 }}
           transition={{ duration: 0.5 }}
           className="w-full lg:w-1/2 lg:p-8"
         >
-          <div className="flex items-center justify-center">
+          <div className="flex items-center justify-center mt-10 sticky top-0 z-40 shadow-md bg-gradient-to-t from-purple-600 to-transparent text-white animate-slide-down">
+            <img className="rounded-2xl relative top-0 z-30" src={aboutImg} alt="about" />
+          </div>
+        </motion.div> */}
+        <motion.div
+          whileInView={{ opacity: 1, x: 0 }}
+          initial={{ x: -100, opacity: 0 }}
+          transition={{ duration: 0.5 }}
+          className="w-full lg:w-1/2 lg:p-8"
+        >
+          <div className="relative flex items-center justify-center mt-0 cursor-pointer group">
+            {/* Image */}
             <img className="rounded-2xl" src={aboutImg} alt="about" />
+
+            {/* Gradient + shadow on hover */}
+            <div className="absolute inset-0 rounded-2xl opacity-0 group-hover:opacity-100 transition-opacity duration-300 shadow-md bg-gradient-to-t from-purple-600 to-transparent"></div>
           </div>
         </motion.div>
+
         <motion.div
           whileInView={{ opacity: 1, x: 0 }}
           initial={{ x: 100, opacity: 0 }}
@@ -27,8 +42,8 @@ const About = () => {
           transition={{ duration: 0.5 }}
           className="w-full lg:w-1/2"
         >
-          <div className="flex justify-center lg:justify-start py-4">
-            <p className="my-2 max-w-xl py-20">{ABOUT_TEXT}</p>
+          <div className="flex justify-center lg:justify-start py-4 lg:py-0">
+            <p className="my-2 lg:my-0 max-w-xl py-20 lg:py-5 lg:mx-8">{ABOUT_TEXT}</p>
           </div>
         </motion.div>
       </div>
